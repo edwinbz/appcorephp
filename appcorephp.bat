70df1b137456c1c2c4f3a252df857bf5309dfc88
Edwin Bautista <b.edwin1999@hotmail.com>
2020-06-10 19:25:10 -0500

    Update tool

---
+++ appcorephp.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+:choice
+set /P c=Do you want to update AppCorePHP? (all files will be replaced) [Y/N]?
+if /I "%c%" EQU "Y" goto :somewhere
+if /I "%c%" EQU "N" goto :somewhere_else
+goto :choice
+
+
+:somewhere
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/commutator.php
+CD ./src/app 
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/database.php
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/functions.php
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/main.php
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/modules.php
+curl -O https://raw.githubusercontent.com/edwinbz/appcorephp/master/router.php
+
+echo ¡AppCorePHP was successfully updated!
+pause
+exit
+
+:somewhere_else
+pause
+exit
---
+++ src/app/functions.php
@@ -0,0 +1,94 @@
+<?php
+// Global App functions
+function _response($success = false, $msg = '', $data = [], $code = 0) // Responder peticiones AJAX con formato JSON
+
+{
+    $response = array(
+        'success' => $success,
+        'code' => $code,
+        'msg' => $msg,
+        'data' => $data,
+    );
+    header("Content-Type: application/json", true);
+    echo json_encode($response);
+    die();
+}
+
+function _val(string $index, $required = true) // Validar variables
+
+{
+    /**
+     * $index = variable
+     * $required = si es un campo requerido por defecto true
+     */
+    $value = isset($_POST[$index]) ? $_POST[$index] : "";
+    if ($required) {
+        if (isset($value) && $value != "") {
+            return $value;
+        } else {
+            if (APP_DEBUG) {
+                _response(false, "Algunos datos requeridos están vacíos ($index)");
+            } else {
+                _response(false, 'Algunos datos requeridos están vacíos');
+            }
+        }
+    }
+    return $value;
+}
+
+function _query($sql, $parameters, $responseType, $operationType = null) // Ejecutar consultas MySQL
+
+{
+    switch ($responseType) {
+        case 1: //FETCH_ASSOC
+            $stm = Conexion::conectar()->prepare($sql);
+            $stm->execute($parameters);
+            return $stm->fetchAll(PDO::FETCH_ASSOC);
+            break;
+        case 2: //FETCH_NUM
+            $stm = Conexion::conectar()->prepare($sql);
+            $stm->execute($parameters);
+            return $stm->fetchAll(PDO::FETCH_NUM);
+            break;
+        case 3: //ROW_COUNT
+            $stm = Conexion::conectar()->prepare($sql);
+            $stm->execute($parameters);
+            return $stm->rowCount();
+            break;
+        case 4: //LAST_ID
+            $con = Conexion::conectar();
+            $stm = $con->prepare($sql);
+            $stm->execute($parameters);
+            return intval($con->lastInsertId());
+            break;
+        default:
+            break;
+    }
+}
+
+function _countTable(string $tableName) // Contar total de registros en una tabla
+
+{
+    $sql = "SELECT COUNT(*) FROM $tableName";
+    return _query($sql, [], RES_FETCH_NUM, OPE_READ)[0][0];
+}
+
+function _log()
+{}
+
+function _isActive($rootLink)
+{
+    if (App::$url[0] == $rootLink) {
+        return 'active';
+    }
+}
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
